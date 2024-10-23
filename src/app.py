dc5c56407052cab045fcd84f0d88a85a86118c09
Mariarodrigues27 <maria.abreulr27@gmail.com>
2024-10-23 23:21:41 +0000

    updates

--- src/app.py
+++ src/app.py
@@ -2,3 +2,275 @@ from utils import db_connect
 engine = db_connect()
 
 # your code here
+
+import pandas as pd
+
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/data-preprocessing-project-tutorial/main/AB_NYC_2019.csv")
+total_data.head()
+
+
+total_data.to_csv("../data/raw/total_data.csv", index = False)
+
+
+# Obtain dimensions
+total_data.shape
+
+
+# Obtain information about data types and non-null values
+total_data.info()
+
+
+print(f"The number of duplicated Name records is: {total_data['name'].duplicated().sum()}")
+print(f"The number of duplicated Host ID records is: {total_data['host_id'].duplicated().sum()}")
+print(f"The number of duplicated ID records is: {total_data['id'].duplicated().sum()}")
+
+
+total_data.drop(["id", "name", "host_name", "last_review", "reviews_per_month"], axis = 1, inplace = True)
+total_data.head()
+
+
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
+fig, axis = plt.subplots(2, 3, figsize=(10, 7))
+
+# Create Histogram
+sns.histplot(ax = axis[0,0], data = total_data, x = "host_id")
+sns.histplot(ax = axis[0,1], data = total_data, x = "neighbourhood_group").set_xticks([])
+sns.histplot(ax = axis[0,2], data = total_data, x = "neighbourhood").set_xticks([])
+sns.histplot(ax = axis[1,0], data = total_data, x = "room_type")
+sns.histplot(ax = axis[1,1], data = total_data, x = "availability_365")
+fig.delaxes(axis[1, 2])
+
+# Adjust layout
+plt.tight_layout()
+
+# Show the plot
+plt.show()
+
+
+fig, axis = plt.subplots(4, 2, figsize = (10, 14), gridspec_kw = {"height_ratios": [6, 1, 6, 1]})
+
+sns.histplot(ax = axis[0, 0], data = total_data, x = "price")
+sns.boxplot(ax = axis[1, 0], data = total_data, x = "price")
+
+sns.histplot(ax = axis[0, 1], data = total_data, x = "minimum_nights").set_xlim(0, 200)
+sns.boxplot(ax = axis[1, 1], data = total_data, x = "minimum_nights")
+
+sns.histplot(ax = axis[2, 0], data = total_data, x = "number_of_reviews")
+sns.boxplot(ax = axis[3, 0], data = total_data, x = "number_of_reviews")
+
+sns.histplot(ax = axis[2,1], data = total_data, x = "calculated_host_listings_count")
+sns.boxplot(ax = axis[3, 1], data = total_data, x = "calculated_host_listings_count")
+
+# Adjust layout
+plt.tight_layout()
+
+# Show the plot
+plt.show()
+
+
+# Numerical - Numerical Analysis
+
+# Create subplot canvas
+fig, axis = plt.subplots(4, 2, figsize = (10, 16))
+
+# Create Plates 
+sns.regplot(ax = axis[0, 0], data = total_data, x = "minimum_nights", y = "price")
+sns.heatmap(total_data[["price", "minimum_nights"]].corr(), annot = True, fmt = ".2f", ax = axis[1, 0], cbar = False)
+
+sns.regplot(ax = axis[0, 1], data = total_data, x = "number_of_reviews", y = "price").set(ylabel = None)
+sns.heatmap(total_data[["price", "number_of_reviews"]].corr(), annot = True, fmt = ".2f", ax = axis[1, 1])
+
+sns.regplot(ax = axis[2, 0], data = total_data, x = "calculated_host_listings_count", y = "price").set(ylabel = None)
+sns.heatmap(total_data[["price", "calculated_host_listings_count"]].corr(), annot = True, fmt = ".2f", ax = axis[3, 0]).set(ylabel = None)
+fig.delaxes(axis[2, 1])
+fig.delaxes(axis[3, 1])
+
+# Adjust layout
+plt.tight_layout()
+
+# Show the plot
+plt.show()
+
+
+fig, axis = plt.subplots(figsize = (5, 4))
+
+sns.countplot(data = total_data, x = "room_type", hue = "neighbourhood_group")
+
+# Show the plot
+plt.show()
+
+
+# Factorize the Room Type and Neighborhood Data
+total_data["room_type"] = pd.factorize(total_data["room_type"])[0]
+total_data["neighbourhood_group"] = pd.factorize(total_data["neighbourhood_group"])[0]
+total_data["neighbourhood"] = pd.factorize(total_data["neighbourhood"])[0]
+
+fig, axes = plt.subplots(figsize=(15, 15))
+
+sns.heatmap(total_data[["neighbourhood_group", "neighbourhood", "room_type", "price", "minimum_nights",	
+                        "number_of_reviews", "calculated_host_listings_count", "availability_365"]].corr(), annot = True, fmt = ".2f")
+
+plt.tight_layout()
+
+# Draw Plot
+plt.show()
+
+
+sns.pairplot(data = total_data)
+
+
+total_data.describe()
+
+
+fig, axes = plt.subplots(3, 3, figsize = (15, 15))
+
+sns.boxplot(ax = axes[0, 0], data = total_data, y = "neighbourhood_group")
+sns.boxplot(ax = axes[0, 1], data = total_data, y = "price")
+sns.boxplot(ax = axes[0, 2], data = total_data, y = "minimum_nights")
+sns.boxplot(ax = axes[1, 0], data = total_data, y = "number_of_reviews")
+sns.boxplot(ax = axes[1, 1], data = total_data, y = "calculated_host_listings_count")
+sns.boxplot(ax = axes[1, 2], data = total_data, y = "availability_365")
+sns.boxplot(ax = axes[2, 0], data = total_data, y = "room_type")
+
+plt.tight_layout()
+
+plt.show()
+
+# Stats for Price
+price_stats = total_data["price"].describe()
+price_stats
+
+
+# IQR for Price
+
+price_iqr = price_stats["75%"] - price_stats["25%"]
+upper_limit = price_stats["75%"] + 1.5 * price_iqr
+lower_limit = price_stats["25%"] - 1.5 * price_iqr
+
+print(f"The upper and lower limits for finding outliers are {round(upper_limit, 2)} and {round(lower_limit, 2)}, with an interquartile range of {round(price_iqr, 2)}")
+
+
+# Clean the outliers
+
+total_data = total_data[total_data["price"] > 0]
+
+
+count_0 = total_data[total_data["price"] == 0].shape[0]
+count_1 = total_data[total_data["price"] == 1].shape[0]
+
+print("Count of 0: ", count_0)
+print("Count of 1: ", count_1)
+
+nights_stats = total_data["minimum_nights"].describe()
+nights_stats
+
+
+# IQR for minimum_nights
+nights_iqr = nights_stats["75%"] - nights_stats["25%"]
+
+upper_limit = nights_stats["75%"] + 1.5 * nights_iqr
+lower_limit = nights_stats["25%"] - 1.5 * nights_iqr
+
+print(f"The upper and lower limits for finding outliers are {round(upper_limit, 2)} and {round(lower_limit, 2)}, with an interquartile range of {round(nights_iqr, 2)}")
+
+# Clean the outliers
+
+total_data = total_data[total_data["minimum_nights"] <= 15]
+
+count_0 = total_data[total_data["minimum_nights"] == 0].shape[0]
+count_1 = total_data[total_data["minimum_nights"] == 1].shape[0]
+count_2 = total_data[total_data["minimum_nights"] == 2].shape[0]
+count_3 = total_data[total_data["minimum_nights"] == 3].shape[0]
+count_4 = total_data[total_data["minimum_nights"] == 4].shape[0]
+
+
+print("Count of 0: ", count_0)
+print("Count of 1: ", count_1)
+print("Count of 2: ", count_2)
+print("Count of 3: ", count_3)
+print("Count of 4: ", count_4)
+
+# Stats for number_of_reviews
+
+review_stats = total_data["number_of_reviews"].describe()
+review_stats
+
+
+# IQR for number_of_reviews
+
+review_iqr = review_stats["75%"] - review_stats["25%"]
+
+upper_limit = review_stats["75%"] + 1.5 * review_iqr
+lower_limit = review_stats["25%"] - 1.5 * review_iqr
+
+print(f"The upper and lower limits for finding outliers are {round(upper_limit, 2)} and {round(lower_limit, 2)}, with an interquartile range of {round(review_iqr, 2)}")
+
+
+
+# Stats for calculated_host_listings_count
+
+hostlist_stats = total_data["calculated_host_listings_count"].describe()
+hostlist_stats
+
+
+# IQR for calculated_host_listings_count
+
+hostlist_iqr = hostlist_stats["75%"] - hostlist_stats["25%"]
+
+upper_limit = hostlist_stats["75%"] + 1.5 * hostlist_iqr
+lower_limit = hostlist_stats["25%"] - 1.5 * hostlist_iqr
+
+print(f"The upper and lower limits for finding outliers are {round(upper_limit, 2)} and {round(lower_limit, 2)}, with an interquartile range of {round(hostlist_iqr, 2)}")
+
+
+count_04 = sum(1 for x in total_data["calculated_host_listings_count"] if x in range(0, 5))
+count_1 = total_data[total_data["calculated_host_listings_count"] == 1].shape[0]
+count_2 = total_data[total_data["calculated_host_listings_count"] == 2].shape[0]
+
+print("Count of 0: ", count_04)
+print("Count of 1: ", count_1)
+print("Count of 2: ", count_2)
+
+# Clean the outliers
+
+total_data = total_data[total_data["calculated_host_listings_count"] > 4]
+
+# Count NaN
+total_data.isnull().sum().sort_values(ascending = False)
+
+
+from sklearn.preprocessing import MinMaxScaler
+
+num_variables = ["number_of_reviews", "minimum_nights", "calculated_host_listings_count", 
+                 "availability_365", "neighbourhood_group", "room_type"]
+scaler = MinMaxScaler()
+scal_features = scaler.fit_transform(total_data[num_variables])
+df_scal = pd.DataFrame(scal_features, index = total_data.index, columns = num_variables)
+df_scal["price"] = total_data["price"]
+df_scal.head()
+
+
+from sklearn.feature_selection import chi2, SelectKBest
+from sklearn.model_selection import train_test_split
+
+X = df_scal.drop("price", axis = 1)
+y = df_scal["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+
+selection_model = SelectKBest(chi2, k = 4)
+selection_model.fit(X_train, y_train)
+ix = selection_model.get_support()
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = X_train.columns.values[ix])
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = X_test.columns.values[ix])
+
+X_train_sel.head()
+
+
+X_train_sel["price"] = list(y_train)
+X_test_sel["price"] = list(y_test)
+X_train_sel.to_csv("../data/processed/clean_train.csv", index = False)
+X_test_sel.to_csv("../data/processed/clean_test.csv", index = False)
